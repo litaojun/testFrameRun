fcd6288eacb51bc17857ef8cd51a021699505e47
li.taojun <li.taojun@LITAOJUN-NB.oriental-pearl.cn>
2016-09-14 18:45:28 +0800

    fffff

--- src/main/java/com/run/TestFrameRunApplication.java
+++ src/main/java/com/run/TestFrameRunApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestFrameRunApplication {
 
 	public static void main(String[] args) {
+		System.out.println("sss");
 		SpringApplication.run(TestFrameRunApplication.class, args);
 	}
 }
